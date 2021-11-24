a9f33d7e6f4c269e2a4ce991924d29d01574ed07
Bruno Carrijo <bruno.carrijo1009@gmail.com>
2021-11-24 20:53:21 -0300

    Criando a classe Posicao

---
+++ Xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace Tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna {  get; set; }    
+
+        public Posicao(int linha, int Coluna)
+        {
+            this.linha = linha;
+            this.coluna = Coluna;   
+        }
+
+        public override string ToString()
+        {
+            return linha
+            + ", "
+            + coluna;
+        }
+    }
+}
